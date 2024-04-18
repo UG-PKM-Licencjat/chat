70210ff7c034c628637adb671bd4dbee1b2dac3f
Maciej Sładkiewicz <m.sladkiewicz.633@studms.ug.edu.pl>
2024-04-18 21:01:21 +0200

    Add basic rest handlers

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,12 @@
 import { FastifyInstance } from "fastify";
 import { handleWs } from "./ws-handler.js";
+import { getMessage, postMessage, getMessages } from "./rest-handler.js";
 
 async function routes(fastify: FastifyInstance) {
   fastify.get("/connect", { websocket: true }, handleWs);
+  fastify.get("/message", getMessage);
+  fastify.get("/messages", getMessages);
+  fastify.post("/messages", postMessage);
 }
 
 export default routes;
