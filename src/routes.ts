019b146686ef31eac6ed1686baa71a47c0f8d783
piotrselak <piotr.selak.it@gmail.com>
2024-06-20 21:26:17 +0200

    Made rest to return conversations, not messages

--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,7 @@ import { getMessage, postMessage, getMessages } from "./rest-handler.js";
 
 async function routes(fastify: FastifyInstance) {
   fastify.get("/connect", { websocket: true }, handleWs);
-  fastify.get("/message", getMessage);
+  fastify.get("/message", getMessage); // TODO: Rework as messages/{id}
   fastify.get("/messages", getMessages);
   fastify.post("/messages", postMessage);
 }
