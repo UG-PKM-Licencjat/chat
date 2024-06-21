5ecc0d4a00a83a59ea925ae4adebd45432417ef7
piotrselak <piotr.selak.it@gmail.com>
2024-06-21 13:10:23 +0200

    Removed one import

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,9 @@
 import { FastifyInstance } from "fastify";
 import { handleWs } from "./ws-handler.js";
-import { getMessage, postMessage, getMessages } from "./rest-handler.js";
+import { postMessage, getMessages } from "./rest-handler.js";
 
 async function routes(fastify: FastifyInstance) {
   fastify.get("/connect", { websocket: true }, handleWs);
-  fastify.get("/message", getMessage); // TODO: Rework as messages/{id}
   fastify.get("/messages", getMessages);
   fastify.post("/messages", postMessage);
 }
