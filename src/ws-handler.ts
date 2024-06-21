a4322399a182fa762109765cbf9e89fac133c6d0
piotrselak <piotr.selak.it@gmail.com>
2024-06-21 14:43:56 +0200

    Fixed id header to querystring

--- src/ws-handler.ts
+++ src/ws-handler.ts
@@ -2,12 +2,15 @@ import { SocketStream } from "@fastify/websocket";
 import { FastifyRequest } from "fastify";
 import { GlobalUserStateHandler } from "./user-state/user-state-handler.js";
 
-export function handleWs(
-  connection: SocketStream,
-  request: FastifyRequest<{ Headers: Record<string, string> }>
-) {
+export function handleWs(connection: SocketStream, request: FastifyRequest) {
   const socket = connection.socket;
-  const { id } = request.headers; // TODO: Name of variable after adding auth
+  const { id } = request.query as Record<string, string>; // TODO: Name of variable after adding auth
+
+  if (!id) {
+    console.log("Missing id, closing connection");
+    socket.close(4001, "Missing id");
+    return;
+  }
 
   console.log("User connected");
 
